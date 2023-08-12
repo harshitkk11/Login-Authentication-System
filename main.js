fbb464b89392daee818d5094ebfa4f7effb1c224
harshitkk11 <114001877+harshitkk11@users.noreply.github.com>
2023-08-12 18:44:07 +0530

    #

---
+++ main.js
@@ -0,0 +1,12 @@
+const username = document.querySelector(".username");
+const password = document.querySelector("#password");
+const submit = document.querySelector(".submit");
+
+
+function findUser(){
+    
+}
+
+
+// on clicking on the login button submit the username and password
+submit.addEventListener('click', findUser);
