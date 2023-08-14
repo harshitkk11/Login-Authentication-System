81b4fa3e8b48e442d3ce494604d29bc5ef0e1291
harshitkk11 <114001877+harshitkk11@users.noreply.github.com>
2023-08-15 01:26:00 +0530

    #

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
 const username = document.querySelector('.username');
 const password = document.querySelector('.password');
+const username_error = document.querySelector('.username-error');
+const password_error = document.querySelector('.password-error');
+const login_error = document.querySelector('.login-error');
+
+login_error.style.display = "none";
+username_error.style.display = "none";
+password_error.style.display = "none";
 
 function validation(){
     if (username.value == "" ){
-        document.querySelector('.alert').textContent = "Enter Username";
+        username_error.style.display = "";
+        login_error.style.display = "none";
+        password_error.style.display = "none";
+        username_error.textContent = "Enter Username";
         return false;
     }
 
     else if (password.value == "" ){
-        document.querySelector('.alert').textContent = "Enter Password";
+        username_error.style.display = "none";
+        login_error.style.display = "none";
+        password_error.style.display = "";
+        password_error.textContent = "Enter Password";
         return false;
     }
     
     else if(username.value != "" || password.value != ""){
-        document.querySelector('.alert').textContent = "";
-        return true;
+        username_error.style.display = "none";
+        login_error.style.display = "";
+        password_error.style.display = "none";
+        login_error.textContent = "User Not Found !!";
+        return false;
     }
+
 
 }
