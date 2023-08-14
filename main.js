e4cbb47c852c1a363b891229d245db1aa7d872e3
harshitkk11 <114001877+harshitkk11@users.noreply.github.com>
2023-08-15 01:58:48 +0530

    #

--- main.js
+++ main.js
@@ -1,37 +1,56 @@
-const username = document.querySelector('.username');
-const password = document.querySelector('.password');
-const username_error = document.querySelector('.username-error');
-const password_error = document.querySelector('.password-error');
+// Login form variables
+const login_username = document.querySelector('.username');
+const login_password = document.querySelector('.password');
+const login_username_error = document.querySelector('.username-error');
+const login_password_error = document.querySelector('.password-error');
 const login_error = document.querySelector('.login-error');
 
 login_error.style.display = "none";
-username_error.style.display = "none";
-password_error.style.display = "none";
+login_username_error.style.display = "none";
+login_password_error.style.display = "none";
+
+// Sign up form variables
+const signup_name = document.querySelector('.name');
+const signup_username = document.querySelector('.username');
+const signup_password = document.querySelector('.password');
+const signup_name_error = document.querySelector('.name-error');
+
+signup_name_error.style.display = "none";
+
+
 
 function loginValidation(){
-    if (username.value == "" ){
-        username_error.style.display = "";
+    if (login_username.value == "" ){
+        login_username_error.style.display = "";
         login_error.style.display = "none";
-        password_error.style.display = "none";
-        username_error.textContent = "Enter Username";
+        login_password_error.style.display = "none";
+        login_username_error.textContent = "Enter Username";
         return false;
     }
 
-    else if (password.value == "" ){
-        username_error.style.display = "none";
+    else if (login_password.value == "" ){
+        login_username_error.style.display = "none";
         login_error.style.display = "none";
-        password_error.style.display = "";
-        password_error.textContent = "Enter Password";
+        login_password_error.style.display = "";
+        login_password_error.textContent = "Enter Password";
         return false;
     }
     
-    else if(username.value != "" || password.value != ""){
-        username_error.style.display = "none";
+    else if(login_username.value != "" || login_password.value != ""){
+        login_username_error.style.display = "none";
         login_error.style.display = "";
-        password_error.style.display = "none";
+        login_password_error.style.display = "none";
         login_error.textContent = "User Not Found !!";
         return false;
     }
 
 
+}
+
+function signupValidation(){
+    if (signup_name.value < 4){
+        signup_name_error.style.display = "";
+        signup_name_error.textContent = "Enter a valid name";
+        return false;
+    }
 }
